827692ae8ca03988388f8fd9b00fd7392ea196b3
Jeremy <594702537@qq.com>
2016-09-30 17:11:13 +0800

    Initial Commit

---
+++ SwizzleDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SwizzleDemo
+//
+//  Created by 萧锐杰 on 16/9/30.
+//  Copyright © 2016年 萧锐杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SwizzleDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SwizzleDemo
+//
+//  Created by 萧锐杰 on 16/9/30.
+//  Copyright © 2016年 萧锐杰. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
